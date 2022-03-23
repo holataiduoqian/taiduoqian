31792a0705866718e9787a84c03768fa1d5fc45c
ssssjdhdhdhdhdbdbrb <52772231+ssssjdhdhdhdhdbdbrb@users.noreply.github.com>
2022-03-23 21:53:02 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -1,5 +1,6 @@
-let targetImage = document.querySelector("#smart-image");
-
+document.addEventListener("DOMContentLoaded", function(event) {
+    let targetImage = document.querySelector("#smart-image");
+});
 targetImage.addEventListener("click", function() {
     targetImage.classList.remove("small");
 });
